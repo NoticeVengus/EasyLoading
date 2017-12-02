c0a554508a030a982cbbf2b4d9c64c865cad7fd2
Ye Ye <0922yeye@163.com>
2017-12-02 13:21:25 +0800

    API modified, container mode supported.

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # EasyLoading
-A loading animation plugin without additional html tags. It is based on jQuery so you need to import it into your project.   
+A loader with animation without additional html tags. It is based on jQuery so you need to import it into your project.   
 You just need to refer to the plug-in and start it directly in the script, loading animation will be shown.  
 Otherwise, timeout and dismiss button is supported. To handle the event? Just set a callback.  
 CSS animation files are referenced from [http://www.lanrenzhijia.com](http://www.lanrenzhijia.com/js/css3/3054.html).
+
+## Preview
+[Click here](https://yeye0922.github.io/EasyLoading) to preview the EasyLoading basic functions in html.  
+[Click here](https://yeye0922.github.io/EasyLoading/advanced) to preview the EasyLoading advanced functions in html. 
+Jquery dialog and container can also shows easy loader as well. This preview using jQueryUI plugin additionally.
 
 ## Version  
 
 | version  | date | detail        | 
 | :-------:|:----:|:-------------:|
-| 0.0.1    | 2017.11.17 | Initial version, basic function      | 
+| 0.1.0    | 2017.11.17 | Initial version, basic function      | 
+| 0.2.0    | 2017.12.1 | Window resize listener, container mode supported      | 
 
 ## Require  
 
@@ -16,8 +22,7 @@ CSS animation files are referenced from [http://www.lanrenzhijia.com](http://www
 | :-------:|:----:|:-------------:|
 | [jQuery](http://jquery.com/)   | 1.6  | DOM operation framework of JavaScript | 
 
-## Usages  
-[Click here](https://yeye0922.github.io/EasyLoading) to preview the EasyLoading in html.  
+## Usages   
 Import 3rd plugin(jQuery).
 
     <script src="js/jquery-1.11.3.min.js"></script>
@@ -38,19 +43,38 @@ Showing the loading animation with some parameters.
 Dismiss button can be used to close the view of animation, or your can finish it manually.  
 
     EasyLoading.hide();
+    
+In version >=0.2.0, container mode is supported which means you can show the loader in a container, such like a div tag or a 
+jqueryUI dialog.  
+Height and width of container should be larger than zero to avoid loader invisible.  
+    
+    EasyLoading.show({
+        text: $("<span>container is loading</span>"),
+        button:$("<span>dismiss</span>")
+    }, $('#container');
+
+Easy loading is running on singleton mode, so only one loader can be shown at the same time. If you want to switch the loader 
+between two different containers, "destroy()" function should be called before fire the "show()" function.
+
+    EasyLoading.destroy();
+
+Destroying the loader means EasyLoading should rebuild struts for a new loader, it will takes extra time compared with fire "show()" function 
+directly. If you just want to show the loader in global mode or in the same container, just call the "show()" function.
      
 ## Parameters  
 EasyLoading.show(params) receives the following parameters.  
 
 | param    | function      | default |
 | :-------:|:-------------:|:------:|
-| type     | Animation type | TYPE.SEMI_CIRCLE_SPIN|
-| timeout      | Loding timeout | _null_|
-| callback    | Callback function | _null_|
+| type     | Animation type | TYPE.BALL_PULSE|
+| timeout  | Loding timeout | _null_|
+| callback  | Callback function | _null_|
 | text      | Loading text be shown| ""|
 | button      | Dismiss button text | _null_|
+| background_color | Loader background color | "black"|
+| dismiss      | Click empty space to dismiss loader | false |
 
-### type
+### 1.type
 Enum below are supported. It determine the type of loading animation.  
 
     TYPE = {
@@ -61,10 +85,10 @@ Enum below are supported. It determine the type of loading animation.
         BALL_SPIN_FADE_LOADER, LINK_SPIN_FADE_LOADER, TRIANGLE_SKEW_SPIN, PACMAN, SEMI_CIRCLE_SPIN
     };
 
-### timeout
+### 2.timeout
 Animation will not be stopped in default. Define this parameter thus the loading process will be terminated after "timeout" milliseconds.
     
-### callback
+### 3.callback
 The callback function triggers when the animation is over or the cancel button is pressed.  
 
     EasyLoading.show({
@@ -75,18 +99,28 @@ The callback function triggers when the animation is over or the cancel button i
     
 | event    | data      | info |
 | :-------:|:-------------:|:------:|
-| 'on_btn_click'     | Event | DOM click event|
+| 'on_btn_click'     | Event | DOM click event for button click|
+|'on_loader_click'     | Event | DOM click event for loader empty body click|
 | 'on_loaded'      | Options | The options from "EasyLoading.show(options)"|
 
-### text
-Animation prompt text. HTML tag is supported.
+### 4.text
+Animation prompt text. HTML tag is supported. You can customized your own text.
 
-### button
-Dismiss button prompt text. HTML tag is supported.
+### 5.button
+Dismiss button prompt text. HTML tag is supported. You can customized your own button.
+
+### 6.background_color
+Defined the loader background color. DOM color tag is supported, or RGB color expression string like "#001122".
+
+### 7.dismiss
+Enable this option to click the empty space of loader for dismiss. If you do not set up the dismiss button, this function should be 
+useful. Callback function will be triggered as usual once you set up.
 
 ## Functions  
 | function    | param      |  return | info |
 | :-------:|:-------------:|:------:|:------:|
-| show(param)     | refer to Parameters above | void | Show loading animation|
-| hide()      | _null_ | void | Dismiss loading animation|
+| show(param)     | refer to Parameters above | void | Show loader|
+| show(param, container)     | refer to Parameters above, jQuery container for showing loader | void | Show loader in container|
+| hide()      | _null_ | void | Dismiss loader|
 | isRunning()    |  _null_ | bool | Whether the loader is processing or not|
+| destroy()     | _null_ | void | Destroy loader|
--- js/easy-loading.js
+++ js/easy-loading.js
@@ -1,15 +1,17 @@
 /*!
  * @author      YeYe
- * @date        2017.11.9
- * @version     0.1.0
+ * @date        2017.12.1
+ * @version     0.2.0
  * @requires
  * jQuery1.6+(http://jquery.com)
  * A float loader with animation, jQuery is required cause the DOM operation is based on it.
+ * https://github.com/yeye0922/EasyLoading
  */
 
 var EasyLoading = (function(){
 
-    var that = {};
+    var that = {}, component={};
+    var options;
 
     that.isInit = false;
 
@@ -24,55 +26,79 @@ var EasyLoading = (function(){
         BALL_PULSE: {name:"ball-pulse", alignCenter:true, offsetY:50, num:3, textOffsetY:40},
         BALL_CLIP_ROTATE: {name:"ball-clip-rotate", alignCenter:true, offsetY:50, num:1, textOffsetY:30},
         BALL_CLIP_ROTATE_PULSE: {name:"ball-clip-rotate-pulse", offsetX:15, offsetY:50, num:2, textOffsetY:40},
-        SQUARE_SPIN: {name:"square-spin", offsetX:25, offsetY:50, num:1, textOffsetY:40},
-        BALL_CLIP_ROTATE_MULTIPLE: {name:"ball-clip-rotate-multiple", offsetX:18, offsetY:50, num:2, textOffsetY:50},
+        SQUARE_SPIN: {name:"square-spin", offsetX:25, offsetY:50, num:1, textOffsetY:40, forceCenter:true},
+        BALL_CLIP_ROTATE_MULTIPLE: {name:"ball-clip-rotate-multiple", offsetX:18, offsetY:50, num:2, textOffsetY:50, forceCenter:true},
         BALL_PULSE_RISE: {name:"ball-pulse-rise", alignCenter:true, offsetY:50, num:5, textOffsetY:50},
-        BALL_ROTATE: {name:"ball-rotate", offsetX:5, offsetY:50, num:1, textOffsetY:50},
-        CUBE_TRANSITION: {name:"cube-transition", offsetX:5, offsetY:60, num:2, textOffsetY:70},
-        BALL_ZIP_ZAG: {name:"ball-zig-zag", offsetX:35, offsetY:80, num:2, textOffsetY:80},
-        BALL_ZIP_ZAG_DEFLECT: {name:"ball-zig-zag-deflect", offsetX:35, offsetY:80, num:2, textOffsetY:80},
-        BALL_TRIANGLE_PATH: {name:"ball-triangle-path", offsetX:5, offsetY:60, num:3, textOffsetY:60},
+        BALL_ROTATE: {name:"ball-rotate", offsetX:5, offsetY:50, num:1, textOffsetY:50, forceCenter:true},
+        CUBE_TRANSITION: {name:"cube-transition", offsetX:5, offsetY:60, num:2, textOffsetY:70, forceCenter:true},
+        BALL_ZIP_ZAG: {name:"ball-zig-zag", offsetX:35, offsetY:80, num:2, textOffsetY:80, forceCenter:true},
+        BALL_ZIP_ZAG_DEFLECT: {name:"ball-zig-zag-deflect", offsetX:35, offsetY:80, num:2, textOffsetY:80, forceCenter:true},
+        BALL_TRIANGLE_PATH: {name:"ball-triangle-path", offsetX:5, offsetY:60, num:3, textOffsetY:60, forceCenter:true},
         BALL_SCALE: {name:"ball-scale", alignCenter:true, offsetY:80, num:1, textOffsetY:10},
         LINE_SCALE: {name:"line-scale", alignCenter:true, offsetY:60, num:5, textOffsetY:10},
         LINE_SCALE_PARTY: {name:"line-scale-party", alignCenter:true, offsetY:60, num:4, textOffsetY:10},
-        BALL_SCALE_MULTIPLE: {name:"ball-scale-multiple", offsetX:30, offsetY:60, num:3, textOffsetY:40},
+        BALL_SCALE_MULTIPLE: {name:"ball-scale-multiple", offsetX:30, offsetY:60, num:3, textOffsetY:40, forceCenter:true},
         BALL_PULSE_SYNC: {name:"ball-pulse-sync", alignCenter:true, offsetY:60, num:3, textOffsetY:10},
         BALL_BEAT: {name:"ball-beat", alignCenter:true, offsetY:60, num:3, textOffsetY:10},
         LINE_SCALE_PULSE_OUT: {name:"line-scale-pulse-out", alignCenter:true, offsetY:50, num:5, textOffsetY:10},
         LINE_SCALE_PULSE_OUT_RAPID: {name:"line-scale-pulse-out-rapid", alignCenter:true, offsetY:50, num:5, textOffsetY:10},
-        BALL_SCALE_RIPPLE: {name:"ball-scale-ripple", offsetX:25, offsetY:50, num:1, textOffsetY:10},
-        BALL_SCALE_RIPPLE_MULTIPLE: {name:"ball-scale-ripple-multiple", offsetX:25, offsetY:20, num:3, textOffsetY:50},
-        BALL_SPIN_FADE_LOADER: {name:"ball-spin-fade-loader", offsetX:10, offsetY:30, num:8, textOffsetY:50},
-        LINK_SPIN_FADE_LOADER: {name:"line-spin-fade-loader", offsetX:4, offsetY:30, num:8, textOffsetY:50},
-        TRIANGLE_SKEW_SPIN: {name:"triangle-skew-spin", offsetX:20, offsetY:30, num:1, textOffsetY:10},
-        PACMAN: {name:"pacman", offsetX:50, offsetY:60, num:5, textOffsetY:10},
-        SEMI_CIRCLE_SPIN: {name:"semi-circle-spin", offsetX:20, offsetY:40, num:1, textOffsetY:20}
+        BALL_SCALE_RIPPLE: {name:"ball-scale-ripple", offsetX:25, offsetY:50, num:1, textOffsetY:10, forceCenter:true},
+        BALL_SCALE_RIPPLE_MULTIPLE: {name:"ball-scale-ripple-multiple", offsetX:25, offsetY:20, num:3, textOffsetY:50, forceCenter:true},
+        BALL_SPIN_FADE_LOADER: {name:"ball-spin-fade-loader", offsetX:10, offsetY:30, num:8, textOffsetY:50, forceCenter:true},
+        LINK_SPIN_FADE_LOADER: {name:"line-spin-fade-loader", offsetX:4, offsetY:30, num:8, textOffsetY:50, forceCenter:true},
+        TRIANGLE_SKEW_SPIN: {name:"triangle-skew-spin", offsetX:20, offsetY:30, num:1, textOffsetY:10, forceCenter:true},
+        PACMAN: {name:"pacman", offsetX:50, offsetY:60, num:5, textOffsetY:10, forceCenter:true},
+        SEMI_CIRCLE_SPIN: {name:"semi-circle-spin", offsetX:20, offsetY:40, num:1, textOffsetY:20, forceCenter:true}
     };
 
     /**
      * Show loader with parameters
      * @param _options
+     * @param container     optional to extra container for loader
      */
-    that.show = function(_options){
-        var options = $.extend({}, defaults, _options);
+    that.show = function(_options, container){
+        options = $.extend({}, defaults, _options);
+        // set up container
+        if(typeof(container)!='undefined'){
+            options.container = container;
+        }
         if(that.isInit){
             initStyle(options);
             $('#easy-loading-main').show();
         }
         else{
-            var main = $("<div id='easy-loading-main' class='easy-loading-main'></div>");
-            var bkg = $("<div class='easy-loading-bkg'></div>");
-            var div = $("<div id='easy-loading-div' class='easy-loading-div'></div>");
-            var content = $("<div id='easy-loading-content'><div></div></div>");
-            var text = $("<div id='easy-loading-text' class='easy-loading-text'></div>");
-            var button = $("<button id='easy-loading-button' class='easy-loading-button'></button>")
-            main.append(bkg).append(div.append(content).append(text).append(button));
-            $("body").append(main);
+            setResizeListener();
+            component.main = $("<div id='easy-loading-main' class='easy-loading-main'></div>");
+            component.bkg = $("<div class='easy-loading-bkg'></div>");
+            component.div = $("<div id='easy-loading-div' class='easy-loading-div'></div>");
+            component.content = $("<div id='easy-loading-content'><div></div></div>");
+            if(typeof(options.text)==='string'){
+                component.text = $("<div id='easy-loading-text' class='easy-loading-text'></div>");
+            }
+            else{
+                component.text = options.text;
+            }
+            if(options.button===null || typeof(options.button)==='string'){
+                component.button = $("<button id='easy-loading-button' class='easy-loading-button'></button>");
+            }
+            else{
+                component.button = options.button;
+            }
+            if(typeof(container)==='undefined'){
+                component.main.css('position', 'fixed');
+                component.main.append(component.bkg).append(component.div.append(component.content).append(component.text).append(component.button));
+                $("body").append(component.main);
+            }
+            else{
+                component.main.css('position', 'relative');
+                component.main.append(component.bkg).append(component.div.append(component.content).append(component.text).append(component.button));
+                container.append(component.main);
+            }
             initStyle(options);
             that.isInit = true;
         }
         // set the timer for expire
-        setTimer(options);
+        setTimer();
     }
 
     /**
@@ -80,70 +106,140 @@ var EasyLoading = (function(){
      * @param options
      */
     var initStyle = function(options){
-        var content = $("#easy-loading-content");
-        var main = $("#easy-loading-main");
-        // 配置显示的文本
-        $("#easy-loading-text").empty().append(options.text).css("padding-top", options.type.textOffsetY);
-        // 配置按钮
+        var content = component.content;
+        var main = component.main;
+        // set up texts
+        if(typeof(options.text)==='string'){
+            component.text.empty().append(options.text).css("padding-top", options.type.textOffsetY);
+        }
+        else{
+            component.text = options.text;
+        }
+        // set up buttons
         if(options.button!=null) {
-            $("#easy-loading-button").empty().append(options.button)
-                .unbind().click(function(e){
-                    that.hide();
-                    if(options.callback!=null)
-                        options.callback('on_btn_click', e);
-                })
-                .show();
+            if(typeof(options.button)==='string'){
+                component.button.empty().append(options.button);
+            }
+            else{
+                component.button = options.button;
+            }
+            component.button.unbind().click(function(e) {
+                that.hide();
+                if (options.callback != null)
+                    options.callback('on_btn_click', e);
+                e.stopPropagation();
+            }).show();
         }
         else
-            $("#easy-loading-button").hide();
-        // 设置class并清空div标签
+            component.button.hide();
+        // set up class and clean <div> tag
         content.find('div').removeClass().addClass(options.type.name).empty();
-        // 添加用于显示的div标签
+        // append <div> tag for displaying
         for(var i=0;i<options.type.num;++i){
             content.find("div").append($("<div></div>"));
         }
-        content.css("margin-top", main.height()/2-options.type.offsetY);
+        // we have to deal with container situation or global situation
+        if(typeof(options.container)!='undefined'){
+            content.css("margin-top", options.container.height()/2-options.type.offsetY);
+        }
+        else{
+            content.css("margin-top", main.height()/2-options.type.offsetY);
+        }
         if(options.type.alignCenter||false) {
-            console.log("Align Center");
             content.css("margin-left", 0);
-            //content.find("div").css("margin-left", main.width() / 2 - options.type.offsetX);
         }
         else {
             content.css("margin-left", main.width() / 2 - options.type.offsetX);
         }
-    }
-
-    that.hide = function(){
-        $('#easy-loading-main').hide();
-        if(typeof(that.timer)!='undefined' && that.timer!=null)
-            clearTimeout(that.timer);
-    }
-
-    that.isRunning = function(){
-        return $("#easy-loading-main").is(":hidden");
+        // click to dismiss
+        if(options.dismiss){
+            component.main.unbind('click');
+            component.main.click(function(e){
+                that.hide();
+                if(options.callback!=null)
+                    options.callback('on_loader_click', e);
+            });
+        }
+        // background color
+        component.bkg.css('background-color', options.background_color);
     }
 
     /**
-     * 配置定时器
+     * hide loader
+     * @param isCallback    trigger callback or not
+     */
+    that.hide = function(isCallback){
+        component.main.hide();
+        if(typeof(that.timer)!='undefined' && that.timer!=null)
+            clearTimeout(that.timer);
+        if(isCallback||true && options.callback!=null)
+            options.callback('on_loaded', options);
+    }
+
+    /**
+     * destroy loader instance
+     */
+    that.destroy = function(){
+        if(that.isInit){
+            that.hide();
+            component.main.unbind();
+            component.main.remove();
+        }
+        component = {};
+        that.isInit = false;
+    }
+
+    /**
+     * loader is running or not
+     * @returns {*}
+     */
+    that.isRunning = function(){
+        if(typeof(component.main)==='undefined')
+            return false;
+        return component.main.is(":hidden");
+    }
+
+    /**
+     * set up timer
      * @param options
      */
-    var setTimer = function(options){
+    var setTimer = function(){
         if(options.timeout>0 && options.timeout!=null){
             that.timer = setTimeout(function () {
-                if(options.callback!=null)
-                    options.callback('on_loaded', options);
-                $('#easy-loading-main').hide();
+                that.hide();
                 that.timer = null;
             }, options.timeout);
         }
     }
 
+    /**
+     * window resize listener
+     * @param options
+     */
+    var setResizeListener = function(){
+        $(window).resize(function(){
+            var main = component.main;
+            var content = component.content;
+            content.css("margin-top", main.height()/2-options.type.offsetY);
+            if(options.type.forceCenter||false){
+                if(options.type.alignCenter||false) {
+                    content.css("margin-left", 0);
+                }
+                else {
+                    content.css("margin-left", main.width() / 2 - options.type.offsetX);
+                }
+            }
+        });
+    }
+
     var defaults = {
-        type: that.TYPE.SEMI_CIRCLE_SPIN,
+        type: that.TYPE.BALL_PULSE,
         timeout: null,
         callback: null,
         text: "",
-        button: null
+        button: null,
+        background_color: "black",
+        dismiss: false
     }
 
     return that;
